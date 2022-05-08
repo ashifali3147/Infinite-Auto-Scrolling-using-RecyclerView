02d787a72bc819a1d37ed827130c654529d02add
Ashif Ali <ashifali3147@gmail.com>
2022-05-08 22:42:35 +0530

    Favourite Button Animation

--- app/src/main/java/com/example/gridview/HomeScreenMenuAdapter.java
+++ app/src/main/java/com/example/gridview/HomeScreenMenuAdapter.java
@@ -2,30 +2,56 @@ package com.example.gridview;
 
 import android.content.Context;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.sackcentury.shinebuttonlib.ShineButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeScreenMenuAdapter extends RecyclerView.Adapter<HomeScreenMenuAdapter.MyviewHolder> {
     Context context;
+
+//    boolean isLiked = false;
     public HomeScreenMenuAdapter(Context context) {
         this.context = context;
     }
-
+    List<Boolean> isLiked = new ArrayList<>();
     @NonNull
     @Override
     public MyviewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_ui_design, viewGroup, false);
         MyviewHolder myviewHolder = new MyviewHolder(view);
+        addData();
         return myviewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyviewHolder myviewHolder, int i) {
+        checkLikeDislike(i, myviewHolder);
+        myviewHolder.like.setOnClickListener(view1 -> {
+            if (isLiked.get(i)){
+                isLiked.add(i,false);
+//                myviewHolder.like.
+                myviewHolder.like.setBtnColor(Color.GRAY);
+                myviewHolder.like.setBtnFillColor(Color.GRAY);
+                Toast.makeText(context, "Dislike", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                isLiked.add(i,true);
+//                myviewHolder.like.setBtnFillColor(Color.parseColor("#673AB7"));
+                Toast.makeText(context, "Like", Toast.LENGTH_SHORT).show();
+            }
 
+        });
     }
 
     @Override
@@ -34,8 +60,40 @@ public class HomeScreenMenuAdapter extends RecyclerView.Adapter<HomeScreenMenuAd
     }
 
     public class MyviewHolder extends RecyclerView.ViewHolder {
+        ShineButton like;
         public MyviewHolder(@NonNull View itemView) {
             super(itemView);
+            like = itemView.findViewById(R.id.like);
+            like.setBtnColor(Color.GRAY);
+            like.setBtnFillColor(Color.parseColor("#673AB7"));
+        }
+    }
+
+    public void addData(){
+        isLiked.add(false);
+        isLiked.add(false);
+        isLiked.add(true);
+        isLiked.add(true);
+        isLiked.add(false);
+        isLiked.add(true);
+        isLiked.add(false);
+        isLiked.add(true);
+        isLiked.add(false);
+        isLiked.add(true);
+    }
+    public void checkLikeDislike(int position, MyviewHolder viewHolder){
+        if (isLiked.get(position)){
+//            isLiked.add(position,false);
+            viewHolder.like.setBtnColor(Color.parseColor("#673AB7"));
+
+//            Toast.makeText(context, "Dislike", Toast.LENGTH_SHORT).show();
+        }
+        else{
+//            isLiked.add(i,true);
+//            viewHolder.like.setBtnFillColor(Color.parseColor("#673AB7"));
+//            Toast.makeText(context, "Like", Toast.LENGTH_SHORT).show();
+
+            viewHolder.like.setBtnColor(Color.GRAY);
         }
     }
 }
--- app/src/main/java/com/example/gridview/MenuScreen.java
+++ app/src/main/java/com/example/gridview/MenuScreen.java
@@ -1,6 +1,8 @@
 package com.example.gridview;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieProperty;
+import com.airbnb.lottie.SimpleColorFilter;
+import com.airbnb.lottie.model.KeyPath;
+import com.airbnb.lottie.value.LottieValueCallback;
+
 public class MenuScreen extends Fragment {
 
     private final Context context;
-
+    boolean isClicked = false;
+    LottieAnimationView like;
     public MenuScreen(Context context) {
         // Required empty public constructor
         this.context = context;
@@ -23,6 +32,39 @@ public class MenuScreen extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_menu_screen, container, false);
+
+        like = view.findViewById(R.id.like);
+//
+
+
+        like.setOnClickListener(view1 -> {
+
+
+
+            if (isClicked){
+                isClicked = false;
+                SimpleColorFilter dislike = new SimpleColorFilter(Color.GRAY);
+                LottieValueCallback<ColorFilter> callback1 = new LottieValueCallback<ColorFilter>(dislike);
+                KeyPath keyPath = new KeyPath("**");
+                like.addValueCallback(keyPath, LottieProperty.COLOR_FILTER, callback1);
+            }
+            else{
+                isClicked = true;
+                SimpleColorFilter cl_like = new SimpleColorFilter(Color.parseColor("#673AB7"));
+                LottieValueCallback<ColorFilter> callback2 = new LottieValueCallback<ColorFilter>(cl_like);
+                KeyPath keyPath2 = new KeyPath("**");
+                like.addValueCallback(keyPath2, LottieProperty.COLOR_FILTER, callback2);
+                like.playAnimation();
+            }
+
+        });
         return view;
     }
 }
+
+/*/
+[Today Work]
+1. Favourite Items Activity, List, Adapter work.
+2. Work on favourite button animation (library & custom).
+
+ */
